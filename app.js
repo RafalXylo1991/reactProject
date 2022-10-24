7d9877347fa5bdf434765fdbfeb060deb7ff995d
xyloh <xylohunter1991@gmail.com>
2022-10-24 21:36:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs")
+const app = express();
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || "200";
+
+
+const router = express.Router();
+app.set("port", port);
+
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/", router);
+
+app.listen(port, () =>{ console.log(`Server running on localhost:${port}`)
+
+
+
+});
