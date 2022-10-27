3a5a941ce516ccb6b5fbbe78677b053b16729571
xyloh <xylohunter1991@gmail.com>
2022-10-27 16:00:59 +0200

    fhjkhjkil;rskt

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs")
 const app = express();
-
+const cors = require("cors");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+import cors from 
 const port = process.env.PORT || "500";
+
 
 
 const router = express.Router();
 app.set("port", port);
 
-
-// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/", router);
+const corsOptions = {
+	origin: 'http://localhost:3000/',
+	optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 router.get("/getData",async(req,res)=>{
 
    
